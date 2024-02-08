43dc9290df301f3f8c370233cb721efd6e227c8e
Matthew Dawber <mgdawber@gmail.com>
2024-02-08 19:18:39 +0000

    feat: Upgrade all dependencies script

--- upgrade-all.sh
+++ upgrade-all.sh
@@ -3,8 +3,31 @@
 # Exit script if any command fails
 set -e
 
-# Update Homebrew
-brew update
+# Logging function
+log() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') $@"
+}
 
-# Upgrade Homebrew
-brew upgrade
+# Update Homebrew
+if command -v brew > /dev/null; then
+    log "Updating Homebrew..."
+    brew update && brew upgrade && brew cleanup
+fi
+
+# Update npm
+if command -v npm > /dev/null; then
+    log "Updating npm packages..."
+    npm update -g
+fi
+
+# Update Ruby Gems
+if command -v gem > /dev/null; then
+    log "Updating Ruby Gems..."
+    gem update --system && gem update
+fi
+
+# Update pip
+if command -v pip > /dev/null; then
+    log "Updating pip packages..."
+    pip install --upgrade pip
+fi
